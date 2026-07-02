4be785bafdbf5b4ded7375fd46b72bf72af879a4
tftt7713-hash <tftt7713@gmail.com>
2026-07-02 19:38:50 +0530

    Update python.py

--- python.py
+++ python.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Kalpvruksha Jadibuti", layout="centered")
-st.title("🤖 Mera AI Chatbot")
+st.title("🤖 Kalpvruksha AI")
 
 # Jo link aapne Chatbase se copy kiya hai, use yahan src ki jagah paste karein
 chatbot_url = "https://www.chatbase.co/chatbot-iframe/Yo9IAwiCy3KAPOyzOWzHC"
