f1e7c2c4e8b405340940be329fc17421068e4534
tftt7713-hash <tftt7713@gmail.com>
2026-07-02 19:11:42 +0530

    Update chatbot URL for iframe integration

--- python.py
+++ python.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Mera Chatbot", layout="centered")
 st.title("🤖 Mera AI Chatbot")
 
 # Jo link aapne Chatbase se copy kiya hai, use yahan src ki jagah paste karein
-chatbot_url = "https://chatbase.co"
+chatbot_url = "https://www.chatbase.co/chatbot-iframe/Yo9IAwiCy3KAPOyzOWzHC"
 
 # Yeh line chatbot ko screen par dikhayegi
 components.iframe(chatbot_url, height=600, scrolling=True)
